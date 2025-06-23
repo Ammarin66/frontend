b13f032a50f96050470c36f302bc24e281742746
Ammarin66 <xamrinthrt7@gmail.com>
2025-06-23 11:35:54 +0700

    a

---
+++ app/service/page.js
@@ -0,0 +1,9 @@
+export default function Service() {
+    return (
+        <>
+        <h1 className="text-4xl text-center text-yellow-500" 
+        >Service page
+        </h1>
+        </>
+    );
+}
