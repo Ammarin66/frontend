b938a16495bf2d651551954bacd02c4d479aff68
Ammarin66 <xamrinthrt7@gmail.com>
2025-07-07 12:44:42 +0700

    Create page.js

---
+++ app/contact/page.js
@@ -0,0 +1,9 @@
+export default function Contact() {
+    return (
+        <>
+        <h1 className="text-4xl text-center text-red-500" 
+        >Contact page
+        </h1>
+        </>
+    );
+}
