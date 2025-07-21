b6556f499e0f55886208ff1e9d9720b1fc458248
Ammarin66 <xamrinthrt7@gmail.com>
2025-07-21 11:21:59 +0700

    Update Navigation.js

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -18,24 +18,27 @@ export default function Navbar() {
           <Link className="nav-link" href="/about">เกี่ยวกับเรา</Link>
         </li>
         <li className="nav-item dropdown">
-          <Link className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <Link className="nav-link dropdown-toggle" href="/service" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             บริการของเรา
           </Link>
           <ul className="dropdown-menu">
-            <li><Link className="dropdown-item" href="#">Action</Link></li>
-            <li><Link className="dropdown-item" href="#">Another action</Link></li>
+            <li><Link className="dropdown-item" href="/service">Action</Link></li>
+            <li><Link className="dropdown-item" href="/service">Another action</Link></li>
             <li><hr className="dropdown-divider" /></li>
-            <li><Link className="dropdown-item" href="#">Something else here</Link></li>
+            <li><Link className="dropdown-item" href="/service">Something else here</Link></li>
           </ul>
         </li>
         <li className="nav-item">
-          <Link href="/" className="nav-link" aria-disabled="true">ติดต่อเรา</Link>
+          <Link href="/contact" className="nav-link" aria-disabled="true">ติดต่อเรา</Link>
         </li>
       </ul>
       <form className="d-flex" role="search">
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
         <button className="btn btn-outline-warning" type="submit">Search</button>
       </form>
+
+ <a href="/login" className="btn btn-primary ms-2">เข้าสู่ระบบ</a>
+
     </div>
   </div>
 </nav>
